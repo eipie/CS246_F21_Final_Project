83562057a2f2ae1adadf49f0227af70886b8c54a
erfan7emz <68251969+erfan7emz@users.noreply.github.com>
2021-11-30 11:13:43 +0330

    erfan

--- king.cc
+++ king.cc
@@ -15,5 +15,66 @@ King::King(Position p, int identifier, bool isFirstMove)
 //      +1v+1h || +1v-1h || -1v+1h || -1v-1h
 // *castling*
 std::vector<PossibleMove> King::getPossibleMoves(const Board & board) {
+    std::vector<PossibleMove> possMoves;
+    int x =pos.x;
+    int y =pos.y;
+    // horizontal moves:
+    // check right
+    Position candidate{x+1,y};
+    // if ther is no king at the following:
+    // (x+2, y), (x+2, y+1), (x+2, y-1)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
 
+    // check left
+    Position candidate{x-1,y};
+    // if ther is no king at the following:
+    // (x-2, y), (x-2, y+1), (x-2, y-1)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    // check up
+    Position candidate{x,y+1};
+    // if ther is no king at the following:
+    // (x, y+2), (x-1, y+2), (x+1, y+2)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    // check down
+    Position candidate{x,y-1};
+    // if ther is no king at the following:
+    // (x, y-2), (x-1, y-2), (x+1, y-2)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    //diagonal moves:
+    // check up right
+    Position candidate{x+1,y+1};
+    // if ther is no king at the following:
+    // (x+2, y), (x+2, y+1), (x+2, y+2), (x+1, y+2), (x, y+2)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    // check up left
+    Position candidate{x-1,y+1};
+    // if ther is no king at the following:
+    // (x-2, y), (x-2, y+1), (x-2, y+2), (x-1, y+2), (x, y+2)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    // check down left
+    Position candidate{x-1,y+1};
+    // if ther is no king at the following:
+    // (x-2, y), (x-2, y-1), (x-2, y-2), (x-1, y-2), (x, y-2)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    // check down right
+    Position candidate{x-1,y+1};
+    // if ther is no king at the following:
+    // (x, y-2), (x+1, y-2), (x+2, y-2), (x+2, y-1), (x+2, y)
+    // then:
+    int tryAddResult = tryAddNextMoveCandidate(board, possMoves, candidate);
+
+    return possMoves;   
 }
--- knight.cc
+++ knight.cc
@@ -14,5 +14,24 @@ Knight::Knight(Position p, int identifier, bool isFirstMove) : ChessPieces(p, id
 //      +1v+2h || +2v+1h || -1v+2h || -2v+1h
 //      +1v-2h || +2v-1h || -1v-2h || -2v-1h
 std::vector<PossibleMove> Knight::getPossibleMoves(const Board & board) {
-
+    std::vector<PossibleMove> possMoves;
+    int x =pos.x;
+    int y =pos.y;
+    Position candidate1{x+1, y+2};
+    Position candidate2{x+2, y+1};
+    Position candidate3{x+2, y-1};
+    Position candidate4{x+1, y-2};
+    Position candidate5{x-1, y+2};
+    Position candidate6{x-2, y+1};
+    Position candidate7{x-2, y-1};
+    Position candidate8{x-1, y-2};
+    // (if x+1 <= 8 && y+2 <= 8) ?????
+    tryAddNextMoveCandidate(board, possMoves, candidate1);
+    tryAddNextMoveCandidate(board, possMoves, candidate2);
+    tryAddNextMoveCandidate(board, possMoves, candidate3);
+    tryAddNextMoveCandidate(board, possMoves, candidate4);
+    tryAddNextMoveCandidate(board, possMoves, candidate5);
+    tryAddNextMoveCandidate(board, possMoves, candidate6);
+    tryAddNextMoveCandidate(board, possMoves, candidate7);
+    tryAddNextMoveCandidate(board, possMoves, candidate8);
 }
